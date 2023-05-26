c125a583cb268a3b23802fd2d1d27dc50661dde4
o <o@DESKTOP-INV86TC>
2023-05-27 00:16:23 +0100

    Add project files.

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,354 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroTextBox1 = new MetroFramework.Controls.MetroTextBox();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.metroButton3 = new MetroFramework.Controls.MetroButton();
+            this.metroButton4 = new MetroFramework.Controls.MetroButton();
+            this.metroButton5 = new MetroFramework.Controls.MetroButton();
+            this.metroButton6 = new MetroFramework.Controls.MetroButton();
+            this.metroButton7 = new MetroFramework.Controls.MetroButton();
+            this.metroButton8 = new MetroFramework.Controls.MetroButton();
+            this.metroButton9 = new MetroFramework.Controls.MetroButton();
+            this.metroButton10 = new MetroFramework.Controls.MetroButton();
+            this.metroButton11 = new MetroFramework.Controls.MetroButton();
+            this.metroButton12 = new MetroFramework.Controls.MetroButton();
+            this.metroButton13 = new MetroFramework.Controls.MetroButton();
+            this.metroButton14 = new MetroFramework.Controls.MetroButton();
+            this.metroButton15 = new MetroFramework.Controls.MetroButton();
+            this.metroButton16 = new MetroFramework.Controls.MetroButton();
+            this.metroButton17 = new MetroFramework.Controls.MetroButton();
+            this.metroButton18 = new MetroFramework.Controls.MetroButton();
+            this.SuspendLayout();
+            // 
+            // metroTextBox1
+            // 
+            this.metroTextBox1.FontSize = MetroFramework.MetroTextBoxSize.Small;
+            this.metroTextBox1.FontWeight = MetroFramework.MetroTextBoxWeight.Regular;
+            this.metroTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.metroTextBox1.Multiline = false;
+            this.metroTextBox1.Name = "metroTextBox1";
+            this.metroTextBox1.SelectedText = "";
+            this.metroTextBox1.Size = new System.Drawing.Size(523, 40);
+            this.metroTextBox1.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBox1.StyleManager = null;
+            this.metroTextBox1.TabIndex = 0;
+            this.metroTextBox1.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBox1.UseStyleColors = false;
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Highlight = false;
+            this.metroButton1.Location = new System.Drawing.Point(12, 58);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(70, 62);
+            this.metroButton1.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroButton1.StyleManager = null;
+            this.metroButton1.TabIndex = 1;
+            this.metroButton1.Text = "1";
+            this.metroButton1.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Highlight = false;
+            this.metroButton2.Location = new System.Drawing.Point(88, 58);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(70, 62);
+            this.metroButton2.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroButton2.StyleManager = null;
+            this.metroButton2.TabIndex = 2;
+            this.metroButton2.Text = "2";
+            this.metroButton2.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // metroButton3
+            // 
+            this.metroButton3.Highlight = false;
+            this.metroButton3.Location = new System.Drawing.Point(164, 58);
+            this.metroButton3.Name = "metroButton3";
+            this.metroButton3.Size = new System.Drawing.Size(70, 62);
+            this.metroButton3.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroButton3.StyleManager = null;
+            this.metroButton3.TabIndex = 3;
+            this.metroButton3.Text = "3";
+            this.metroButton3.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroButton3.Click += new System.EventHandler(this.metroButton3_Click);
+            // 
+            // metroButton4
+            // 
+            this.metroButton4.Highlight = false;
+            this.metroButton4.Location = new System.Drawing.Point(12, 126);
+            this.metroButton4.Name = "metroButton4";
+            this.metroButton4.Size = new System.Drawing.Size(70, 62);
+            this.metroButton4.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroButton4.StyleManager = null;
+            this.metroButton4.TabIndex = 4;
+            this.metroButton4.Text = "4";
+            this.metroButton4.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroButton4.Click += new System.EventHandler(this.metroButton4_Click);
+            // 
+            // metroButton5
+            // 
+            this.metroButton5.Highlight = false;
+            this.metroButton5.Location = new System.Drawing.Point(88, 126);
+            this.metroButton5.Name = "metroButton5";
+            this.metroButton5.Size = new System.Drawing.Size(70, 62);
+            this.metroButton5.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroButton5.StyleManager = null;
+            this.metroButton5.TabIndex = 5;
+            this.metroButton5.Text = "5";
+            this.metroButton5.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroButton5.Click += new System.EventHandler(this.metroButton5_Click);
+            // 
+            // metroButton6
+            // 
+            this.metroButton6.Highlight = false;
+            this.metroButton6.Location = new System.Drawing.Point(164, 126);
+            this.metroButton6.Name = "metroButton6";
+            this.metroButton6.Size = new System.Drawing.Size(70, 62);
+            this.metroButton6.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroButton6.StyleManager = null;
+            this.metroButton6.TabIndex = 6;
+            this.metroButton6.Text = "6";
+            this.metroButton6.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroButton6.Click += new System.EventHandler(this.metroButton6_Click);
+            // 
+            // metroButton7
+            // 
+            this.metroButton7.Highlight = false;
+            this.metroButton7.Location = new System.Drawing.Point(12, 194);
+            this.metroButton7.Name = "metroButton7";
+            this.metroButton7.Size = new System.Drawing.Size(70, 62);
+            this.metroButton7.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroButton7.StyleManager = null;
+            this.metroButton7.TabIndex = 7;
+            this.metroButton7.Text = "7";
+            this.metroButton7.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroButton7.Click += new System.EventHandler(this.metroButton7_Click);
+            // 
+            // metroButton8
+            // 
+            this.metroButton8.Highlight = false;
+            this.metroButton8.Location = new System.Drawing.Point(88, 194);
+            this.metroButton8.Name = "metroButton8";
+            this.metroButton8.Size = new System.Drawing.Size(70, 62);
+            this.metroButton8.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroButton8.StyleManager = null;
+            this.metroButton8.TabIndex = 8;
+            this.metroButton8.Text = "8";
+            this.metroButton8.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroButton8.Click += new System.EventHandler(this.metroButton8_Click);
+            // 
+            // metroButton9
+            // 
+            this.metroButton9.Highlight = false;
+            this.metroButton9.Location = new System.Drawing.Point(164, 194);
+            this.metroButton9.Name = "metroButton9";
+            this.metroButton9.Size = new System.Drawing.Size(70, 62);
+            this.metroButton9.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroButton9.StyleManager = null;
+            this.metroButton9.TabIndex = 9;
+            this.metroButton9.Text = "9";
+            this.metroButton9.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroButton9.Click += new System.EventHandler(this.metroButton9_Click);
+            // 
+            // metroButton10
+            // 
+            this.metroButton10.Highlight = false;
+            this.metroButton10.Location = new System.Drawing.Point(88, 262);
+            this.metroButton10.Name = "metroButton10";
+            this.metroButton10.Size = new System.Drawing.Size(70, 62);
+            this.metroButton10.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroButton10.StyleManager = null;
+            this.metroButton10.TabIndex = 10;
+            this.metroButton10.Text = "0";
+            this.metroButton10.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroButton10.Click += new System.EventHandler(this.metroButton10_Click);
+            // 
+            // metroButton11
+            // 
+            this.metroButton11.Highlight = false;
+            this.metroButton11.Location = new System.Drawing.Point(240, 262);
+            this.metroButton11.Name = "metroButton11";
+            this.metroButton11.Size = new System.Drawing.Size(70, 62);
+            this.metroButton11.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroButton11.StyleManager = null;
+            this.metroButton11.TabIndex = 11;
+            this.metroButton11.Text = "-";
+            this.metroButton11.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroButton11.Click += new System.EventHandler(this.metroButton11_Click);
+            // 
+            // metroButton12
+            // 
+            this.metroButton12.Highlight = false;
+            this.metroButton12.Location = new System.Drawing.Point(164, 262);
+            this.metroButton12.Name = "metroButton12";
+            this.metroButton12.Size = new System.Drawing.Size(70, 62);
+            this.metroButton12.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroButton12.StyleManager = null;
+            this.metroButton12.TabIndex = 12;
+            this.metroButton12.Text = "+";
+            this.metroButton12.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroButton12.Click += new System.EventHandler(this.metroButton12_Click);
+            // 
+            // metroButton13
+            // 
+            this.metroButton13.Highlight = false;
+            this.metroButton13.Location = new System.Drawing.Point(389, 262);
+            this.metroButton13.Name = "metroButton13";
+            this.metroButton13.Size = new System.Drawing.Size(146, 62);
+            this.metroButton13.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroButton13.StyleManager = null;
+            this.metroButton13.TabIndex = 13;
+            this.metroButton13.Text = "=";
+            this.metroButton13.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroButton13.Click += new System.EventHandler(this.metroButton13_Click);
+            // 
+            // metroButton14
+            // 
+            this.metroButton14.Highlight = false;
+            this.metroButton14.Location = new System.Drawing.Point(240, 58);
+            this.metroButton14.Name = "metroButton14";
+            this.metroButton14.Size = new System.Drawing.Size(70, 62);
+            this.metroButton14.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroButton14.StyleManager = null;
+            this.metroButton14.TabIndex = 14;
+            this.metroButton14.Text = "×";
+            this.metroButton14.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroButton14.Click += new System.EventHandler(this.metroButton14_Click);
+            // 
+            // metroButton15
+            // 
+            this.metroButton15.Highlight = false;
+            this.metroButton15.Location = new System.Drawing.Point(389, 58);
+            this.metroButton15.Name = "metroButton15";
+            this.metroButton15.Size = new System.Drawing.Size(146, 62);
+            this.metroButton15.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroButton15.StyleManager = null;
+            this.metroButton15.TabIndex = 15;
+            this.metroButton15.Text = "<-";
+            this.metroButton15.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroButton15.Click += new System.EventHandler(this.metroButton15_Click);
+            // 
+            // metroButton16
+            // 
+            this.metroButton16.Highlight = false;
+            this.metroButton16.Location = new System.Drawing.Point(240, 126);
+            this.metroButton16.Name = "metroButton16";
+            this.metroButton16.Size = new System.Drawing.Size(70, 62);
+            this.metroButton16.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroButton16.StyleManager = null;
+            this.metroButton16.TabIndex = 16;
+            this.metroButton16.Text = "÷";
+            this.metroButton16.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroButton16.Click += new System.EventHandler(this.metroButton16_Click);
+            // 
+            // metroButton17
+            // 
+            this.metroButton17.Highlight = false;
+            this.metroButton17.Location = new System.Drawing.Point(240, 194);
+            this.metroButton17.Name = "metroButton17";
+            this.metroButton17.Size = new System.Drawing.Size(70, 62);
+            this.metroButton17.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroButton17.StyleManager = null;
+            this.metroButton17.TabIndex = 17;
+            this.metroButton17.Text = "^";
+            this.metroButton17.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroButton17.Click += new System.EventHandler(this.metroButton17_Click);
+            // 
+            // metroButton18
+            // 
+            this.metroButton18.Highlight = false;
+            this.metroButton18.Location = new System.Drawing.Point(12, 260);
+            this.metroButton18.Name = "metroButton18";
+            this.metroButton18.Size = new System.Drawing.Size(70, 62);
+            this.metroButton18.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroButton18.StyleManager = null;
+            this.metroButton18.TabIndex = 18;
+            this.metroButton18.Text = ".";
+            this.metroButton18.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroButton18.Click += new System.EventHandler(this.metroButton18_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 334);
+            this.Controls.Add(this.metroButton18);
+            this.Controls.Add(this.metroButton17);
+            this.Controls.Add(this.metroButton16);
+            this.Controls.Add(this.metroButton15);
+            this.Controls.Add(this.metroButton14);
+            this.Controls.Add(this.metroButton13);
+            this.Controls.Add(this.metroButton12);
+            this.Controls.Add(this.metroButton11);
+            this.Controls.Add(this.metroButton10);
+            this.Controls.Add(this.metroButton9);
+            this.Controls.Add(this.metroButton8);
+            this.Controls.Add(this.metroButton7);
+            this.Controls.Add(this.metroButton6);
+            this.Controls.Add(this.metroButton5);
+            this.Controls.Add(this.metroButton4);
+            this.Controls.Add(this.metroButton3);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.metroTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroTextBox metroTextBox1;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroButton metroButton2;
+        private MetroFramework.Controls.MetroButton metroButton3;
+        private MetroFramework.Controls.MetroButton metroButton4;
+        private MetroFramework.Controls.MetroButton metroButton5;
+        private MetroFramework.Controls.MetroButton metroButton6;
+        private MetroFramework.Controls.MetroButton metroButton7;
+        private MetroFramework.Controls.MetroButton metroButton8;
+        private MetroFramework.Controls.MetroButton metroButton9;
+        private MetroFramework.Controls.MetroButton metroButton10;
+        private MetroFramework.Controls.MetroButton metroButton11;
+        private MetroFramework.Controls.MetroButton metroButton12;
+        private MetroFramework.Controls.MetroButton metroButton13;
+        private MetroFramework.Controls.MetroButton metroButton14;
+        private MetroFramework.Controls.MetroButton metroButton15;
+        private MetroFramework.Controls.MetroButton metroButton16;
+        private MetroFramework.Controls.MetroButton metroButton17;
+        private MetroFramework.Controls.MetroButton metroButton18;
+    }
+}
+
---
+++ Calculator/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        double term1 = 0;
+        double term2 = 0;
+        double result = 0;
+        double operation = 0; 
+        /* 
+            1 for addition 
+            2 for subtraction
+            3 for multiplication
+            4 for division
+            5 for powers
+        */
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            metroTextBox1.Text += "1";
+        }
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            metroTextBox1.Text += "2";
+        }
+        private void metroButton3_Click(object sender, EventArgs e)
+        {
+            metroTextBox1.Text += "3";
+        }
+        private void metroButton4_Click(object sender, EventArgs e)
+        {
+            metroTextBox1.Text += "4";
+        }
+        private void metroButton5_Click(object sender, EventArgs e)
+        {
+            metroTextBox1.Text += "5";
+        }
+        private void metroButton6_Click(object sender, EventArgs e)
+        {
+            metroTextBox1.Text += "6";
+        }
+        private void metroButton7_Click(object sender, EventArgs e)
+        {
+            metroTextBox1.Text += "7";
+        }
+        private void metroButton8_Click(object sender, EventArgs e)
+        {
+            metroTextBox1.Text += "8";
+        }
+        private void metroButton9_Click(object sender, EventArgs e)
+        {
+            metroTextBox1.Text += "9";
+        }
+
+        private void metroButton10_Click(object sender, EventArgs e)
+        {
+            metroTextBox1.Text += "0";
+        }
+
+        private void metroButton15_Click(object sender, EventArgs e)
+        {
+            if (metroTextBox1.Text.Length > 0)
+            {
+                metroTextBox1.Text = metroTextBox1.Text.Substring(0, metroTextBox1.Text.Length - 1);
+            } else
+            {
+                MessageBox.Show("No number.", "Error");
+            }
+            
+        }
+
+        private void metroButton12_Click(object sender, EventArgs e)
+        {
+            if (!Double.TryParse(metroTextBox1.Text, out term1))
+            {
+                MessageBox.Show("Invalid Characters.", "Error");
+            }
+
+            operation = 1;
+            
+            metroTextBox1.Text = "";
+        }
+
+        private void metroButton11_Click(object sender, EventArgs e)
+        {
+            if (!Double.TryParse(metroTextBox1.Text, out term1))
+            {
+                MessageBox.Show("Invalid Characters.", "Error");
+            }
+
+            operation = 2;
+
+            metroTextBox1.Text = "";
+        }
+
+        private void metroButton14_Click(object sender, EventArgs e)
+        {
+            if (!Double.TryParse(metroTextBox1.Text, out term1))
+            {
+                MessageBox.Show("Invalid Characters.", "Error");
+            }
+
+            operation = 3;
+
+            metroTextBox1.Text = "";
+        }
+
+        private void metroButton16_Click(object sender, EventArgs e)
+        {
+            if (!Double.TryParse(metroTextBox1.Text, out term1))
+            {
+                MessageBox.Show("Invalid Characters.", "Error");
+            }
+
+            operation = 4;
+
+            metroTextBox1.Text = "";
+        }
+
+        private void metroButton13_Click(object sender, EventArgs e)
+        {
+            if (!Double.TryParse(metroTextBox1.Text, out term2))
+            {
+                MessageBox.Show("Invalid Characters.", "Error");
+            }
+            switch (operation)
+            {
+                case 0:
+                    metroTextBox1.Text = term1.ToString("F");
+                    break;
+                case 1:
+                    result = term1 + term2;
+                    metroTextBox1.Text = result.ToString("F");
+                    break;
+
+                case 2:
+                    result = term1 - term2;
+                    metroTextBox1.Text = result.ToString("F");
+                    break;
+
+                case 3:
+                    result = term1 * term2;
+                    metroTextBox1.Text = result.ToString("F");
+                    break;
+
+                case 4:
+                    if (term2 == 0) { MessageBox.Show("Cannot divide by zero.", "Error"); }
+                    result = term1 / term2;
+                    metroTextBox1.Text = result.ToString("F");
+                    break;
+
+                case 5:
+                    result = Math.Pow(term1, term2);
+                    metroTextBox1.Text = result.ToString("F");
+                    break;
+            }
+
+            term2 = 0;
+
+        }
+
+        private void metroButton17_Click(object sender, EventArgs e)
+        {
+            if (!Double.TryParse(metroTextBox1.Text, out term1))
+            {
+                MessageBox.Show("Invalid Characters.", "Error");
+            }
+
+            operation = 5;
+
+            metroTextBox1.Text = "";
+        }
+
+        private void metroButton18_Click(object sender, EventArgs e)
+        {
+            metroTextBox1.Text += ".";
+        }
+    }
+}
